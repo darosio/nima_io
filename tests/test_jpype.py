08a7507357cac6e8f92cb6376fd75c7b1cfdd174
daniele arosio <daniele.arosio@cnr.it>
2018-08-13 14:11:06 +0200

    fix convertion jpype failures; blaken and pylint

--- tests/test_jpype.py
+++ tests/test_jpype.py
@@ -1,9 +1,28 @@
+"""
+Module to test methods based on jpype1.
+"""
 import pytest
 import imgread.read as ir
 from test_read import check_core_md, check_single_md
 
 
-@pytest.mark.jpype
+@pytest.mark.myjpype
+class TestJpype:
+    """Test both metadata and data with all files, OME and LIF, using
+    javabridge OMEXmlMetadata into bioformats image reader.
+
+    """
+
+    def setup_class(cls):
+        cls.read = ir.read_jpype
+
+    def test_metadata_data(self, read_all):
+        test_d, md, wrapper = read_all
+        check_core_md(md, test_d)
+        # check_data(wrapper, test_d['data'])
+
+
+@pytest.mark.pims
 class TestPims:
     """Test both metadata and data with all files, OME and LIF, using
     javabridge OMEXmlMetadata into bioformats image reader.
@@ -18,19 +37,24 @@ class TestPims:
         check_core_md(md, test_d)
         # check_data(wrapper, test_d['data'])
 
-    @pytest.mark.parametrize('key', [
-        'SizeS',
-        'SizeX',
-        'SizeY',
-        'SizeC',
-        'SizeT',
-        'SizeZ',
-        pytest.param(
-            'PhysicalSizeX',
-            marks=pytest.mark.xfail(
-                raises=AssertionError,
-                reason="Probably PIMS uses wrong double/float convertion")),
-    ])
+    @pytest.mark.parametrize(
+        "key",
+        [
+            "SizeS",
+            "SizeX",
+            "SizeY",
+            "SizeC",
+            "SizeT",
+            "SizeZ",
+            pytest.param(
+                "PhysicalSizeX",
+                marks=pytest.mark.xfail(
+                    raises=AssertionError,
+                    reason="loci 5.7.0 divides for SizeX instead of SizeX-1",
+                ),
+            ),
+        ],
+    )
     def test_metadata_data_LIF(self, read_LIF, key):
         test_d, md, wrapper = read_LIF
         check_single_md(md, test_d, key)
